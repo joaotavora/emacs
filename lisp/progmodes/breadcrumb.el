829af12c3798b49333017c76d91425177e174838
João Távora <joaotavora@gmail.com>
2023-04-17 01:33:58 +0100

    slightly better

--- lisp/progmodes/breadcrumb.el
+++ lisp/progmodes/breadcrumb.el
@@ -26,26 +26,32 @@
 (require 'cl-lib)
 (require 'imenu)
 
+(cl-defun bc-bisect (a x &key (from 0) (to (length a)) key from-end)
+  "Compute index to insert X in sequence A, keeping it sorted.
+If X already in A, the resulting index is the leftmost such
+index, unless FROM-END is t.  KEY is as usual in other CL land."
+  (cl-macrolet ((search (from-end key)
+                  `(cl-loop while (< from to)
+                            for mid = (/ (+ from to) 2)
+                            for p1 = (elt a mid)
+                            for p2 = ,(if key `(funcall key p1) `p1)
+                            if (,(if from-end '< '<=) x p2)
+                            do (setq to mid) else do (setq from (1+ mid))
+                            finally return from)))
+    (if from-end (if key (search t key) (search t nil))
+      (if key (search nil key) (search nil nil)))))
+
 (defvar-local bc--path-1-cache nil)
 (defun bc--path-1 (index-alist pos)
-  (cl-labels ((dfs (n &optional path)
-                (setq path (cons (car n) path))
-                (if (consp (cdr n))
-                    (mapc (lambda (n) (dfs n path)) (cdr n))
-                  (setq bc--path-1-cache
-                        (vconcat bc--path-1-cache
-                                 `[,(cons (cdr n) path)])))))
-    (unless bc--path-1-cache
-      (mapc #'dfs index-alist)
-      (setq bc--path-1-cache (cl-sort bc--path-1-cache #'< :key #'car)))
-    (unless (< pos (car (aref bc--path-1-cache 0)))
-      (cl-loop with l = bc--path-1-cache
-               with min = 0 with max = (length l)
-               for i = (+ min (/ (- max min) 2))
-               for x = (elt l i)
-               if (< pos (car x)) do (setq max i)
-               else do (setq min i)
-               if (= min (1- max)) return (reverse (cdr (elt l min)))))))
+  (cl-labels
+      ((search (nodes &optional path)
+         (cl-loop
+          for n in nodes
+          for reg = (get-text-property 0 'breadcrumb-region (car n))
+          when (<= (car reg) pos (cdr reg))
+          return (search (cdr n) (cons (car n) path))
+          finally (cl-return path))))
+    (nreverse (search index-alist))))
 
 (defvar-local bc--path-2-cache nil)
 (defun bc--path-2 (index-alist pos)
@@ -60,19 +66,8 @@
       (mapc #'dfs index-alist)
       (setq bc--path-2-cache (cl-sort bc--path-2-cache #'< :key #'car)))
     (unless (< pos (car (aref bc--path-2-cache 0)))
-      (cl-loop with l = bc--path-2-cache
-               with min = 0 with max = (length l)
-               for i = (+ min (/ (- max min) 2))
-               for x = (elt l i)
-               if (< pos (car x)) do (setq max i)
-               else do (setq min i)
-               if (= min (1- max)) return (reverse (cdr (elt l min)))))))
-
-;; (defun bc--bench-2 ()
-;;   (progn (setq bc--path-2-cache nil)
-;;          (imenu--make-index-alist)
-;;          (benchmark-run 100000 (bc--path-2 imenu--index-alist (point)))))
-
+      (let ((res (bc-bisect bc--path-2-cache pos :key #'car :from-end t)))
+        (unless (zerop res) (reverse (cdr (elt bc--path-2-cache (1- res)))))))))
 
 (defun bc-path (index-alist pos)
   "Get breadcrumb for position POS given INDEX-ALIST."
@@ -118,9 +113,31 @@
   (if bc-mode (add-to-list 'header-line-format bc-header-line-format)
     (setq header-line-format (delq bc-header-line-format header-line-format))))
 
+(defun bc-jump ()
+  (interactive)
+  (let (cands choice)
+    (cl-labels
+        ((fmt (strs)
+           (mapconcat #'identity strs " > "))
+         (dfs (nodes &optional path)
+           (cl-loop
+            for n in nodes
+            for pos = (or (car (get-text-property 0 'breadcrumb-region (car n)))
+                          (and (number-or-marker-p (cdr n)) (cdr n)))
+            when pos do (push (cons (fmt (reverse path)) pos)
+                              cands)
+            do (dfs (cdr n) path))))
+      (imenu--make-index-alist)
+      (dfs imenu--index-alist)
+      (unless cands (user-error "Sorry, no breadcrumb items to jump to."))
+      (setq choice (cdr (assoc (completing-read "Index item? " cands nil t)
+                               cands #'string=)))
+      (push-mark)
+      (goto-char choice))))
+
 (provide 'breadcrumb)
 ;;; breadcrumb.el ends here
 
 ;; Local Variables:
-;; read-symbol-shorthands: (("bc-" . "breadcrumb-"))
+;; read-symbol-shorthands: (("bc-" . "breadcrumb-") ("s-" . "breadcrumb-segment-tree-"))
 ;; End:
